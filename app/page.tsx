2d5be21a73db1bb416a3f03f1b51c5f39b7d9060
gong8 <gonglx8@gmail.com>
2024-03-06 21:29:24 +0000

    UI update + graph API implementation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import axios from 'axios';
 import { useEffect,  useState } from 'react';
 import dynamic from 'next/dynamic';
+
 const CanvasJSChart = dynamic(
   async () => {
     const {
@@ -14,40 +15,97 @@ const CanvasJSChart = dynamic(
 { ssr: false }
 );
 
+const denyPolygon =  false;
+
 const instance = axios.create(
-  {baseURL: './api'}
+  {
+    baseURL: './api',
+  },
+)
+
+const polygon = axios.create(
+  {
+    baseURL: 'https://api.polygon.io/v2/',
+    headers: {'Authorization': 'Bearer 11Rhj8iFjTgczZ_mkeJK5r5jz4XHlLcj'},
+  }
 )
 
 
 
 export default function Home() {
-  const [data, setData] = useState<string | null>(null)
-  useEffect(() => {
-    instance.get('/index')
-      .then((response) => {
-        setData(response.data);
-      })
-  })
+  const [data, setData] = useState<string | null>(null);
+  const [options, setOptions] = useState<any>(null);
+  const [graphData, setGraphData] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
-  const options = {
-    theme: "light2",
-    title: {
-      text: "Nifty 50 Index"
-    },
-    data: [{
-      type: "line",
-      xValueFormatString: "MMM YYYY",
-      yValueFormatString: "#,##0.00",
-      dataPoints: [{x: new Date(Date.now()), y: 0}]
-    }]
+  useEffect(() => {
+    setLoading(true);
+    if (denyPolygon) {
+      return;
+
+    }
+    const indicesTicker = "AAPL";
+    const multiplier = 10;
+    const timespan = "minute";
+    const from = "2023-01-03";
+    const to = "2023-01-03";
+
+    polygon.get(`aggs/ticker/${indicesTicker}/range/${multiplier}/${timespan}/${from}/${to}`).then((res) => {
+      setGraphData(res.data);
+    }).catch((err) => {
+      console.log("error", err);
+    }).finally(() => {
+      setLoading(false);
+
+
+    })
+
+  }, [])
+
+
+  useEffect(() => {
+    updateGraph(graphData);
+  },[graphData])
+
+  function updateGraph(data: any) {
+    let newDataPoints = [];
+    if (data) {
+      for (const dict of data.results) {
+        // [Open, High, Low, Close]
+        const [open, high, low, close] = [dict.o, dict.h, dict.l, dict.c]
+        const time = new Date(dict.t);
+        const point = {x: time, y: [open, high, low, close]};
+        newDataPoints.push(point);
+      }
+    }
+    setOptions({
+      theme: "dark2",
+      backgroundColor: "#000000",
+      title: {
+        text: ""
+      },
+      data: [{
+        type: "candlestick",
+        xValueFormatString: "MMM YYYY",
+        yValueFormatString: "#,##0.00",
+        dataPoints: newDataPoints,
+      }]
+    });
   }
 
   return (
     <main>
-      <h1>Home</h1>
-      <p>{data}</p>
-      {/*@ts-ignore*/}
-      <CanvasJSChart options = {options} />
+      <div className={styles.root}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>trademaker</h1>
+          <div className={`${styles.loadingBar} ${loading ? styles.loading : ""}`}></div>
+        </div>
+
+        <p>{data}</p>
+        {/*@ts-ignore*/}
+        <CanvasJSChart options = {options} />
+
+      </div>
     </main>
   );
 }
